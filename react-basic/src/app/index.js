4f55be9757c59f5b6a2411ec8081a1b6ef358d88
Bhavdeep <dhanjalbhavdeep@gmail.com>
2018-01-16 19:33:50 +0530

    Explored basics of props and state

--- react-basic/src/app/index.js
+++ react-basic/src/app/index.js
@@ -7,17 +7,60 @@ const myCheese = {
     price: '3.50',
 }
 
-// create component
-const Welcome = (props) => {
-    return (
-        <div>
-            <h1>Hello, {props.name}, {props.message}</h1>
-            <p>Cheese Name: {props.cheese.name}</p>
-            <p>Smell Factor: {props.cheese.smellFactor}</p>
-            <p>Price: {props.cheese.price}</p>
-        </div>
-)}
-const element = <Welcome name="Sara" message="I like Cheese" cheese={myCheese}/>
+// create component via a function
+// function Welcome (props){
+//     return (
+//         <div>
+            
+//         </div>
+// )}
+
+// ways to pass properties
+// const element = <Welcome name="Sara" message="I like Cheese" cheese={myCheese}/>
+
+// creating a component using a class
+// look into HOCs for doing away with classes
+class TodoComponent extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            todos: ['Wake up', 'Shake up', 'Pick up keys'],
+            age: 22
+        }
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                age: 23
+            })
+        }, 1000)
+    }
+
+    render() {
+
+        return (
+            <div id="todo-list">
+                <p>The busiest people have the most leisure...</p>
+                <p>{this.state.age}</p>
+                <ul>
+                    <li>{this.state.todos[0]}</li>
+                    <li>{this.state.todos[1]}</li>
+                    <li>{this.state.todos[2]}</li>
+                </ul>
+                <ListComponent todos={this.state.todos} />
+            </div>
+        )
+    }
+}
+
+class ListComponent extends React.Component {
+    render() {
+        return (
+            <p>{this.props.todos[0]}</p>
+        )
+    }
+}
 
 // put component into html page
-ReactDOM.render(element, document.getElementById('todo-wrapper'))
+ReactDOM.render(<TodoComponent />, document.getElementById('todo-wrapper'))
