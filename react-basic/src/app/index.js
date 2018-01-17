ab7893047457d1bb1e634b667aa6fb48d18c47f9
Bhavdeep <dhanjalbhavdeep@gmail.com>
2018-01-17 20:08:09 +0530

    Learnt child parent events handling

--- react-basic/src/app/index.js
+++ react-basic/src/app/index.js
@@ -23,6 +23,7 @@ const myCheese = {
 class TodoComponent extends React.Component {
     constructor(props) {
         super(props)
+        this.onDelete = this.onDelete.bind(this)
         this.state = {
             todos: ['Wake up', 'Shake up', 'Pick up keys'],
             age: 22
@@ -39,18 +40,55 @@ class TodoComponent extends React.Component {
 
     render() {
 
+        let todos = this.state.todos
+        todos = todos.map((item, index) => {
+            return (
+                <TodoItem item={item} key={index} onDelete={this.onDelete} />
+            )
+        })
+
         return (
             <div id="todo-list">
                 <p>The busiest people have the most leisure...</p>
                 <p>{this.state.age}</p>
-                <ul>
-                    <li>{this.state.todos[0]}</li>
-                    <li>{this.state.todos[1]}</li>
-                    <li>{this.state.todos[2]}</li>
-                </ul>
+                <ul>{todos}</ul>
                 <ListComponent todos={this.state.todos} />
             </div>
         )
+    }
+
+    onDelete(item) {
+        let updatedTodos = this.state.todos.filter(function(val, index) {
+            return item !== val
+        })
+        this.setState({
+            todos: updatedTodos
+        })
+    }
+}
+
+// create TodoItem component
+
+class TodoItem extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.handleDelete = this.handleDelete.bind(this)
+    }
+
+    render() {
+        return (
+            <li>
+                <div className="todo-item">
+                    <span className="item-name" onClick={this.test}>{this.props.item}</span>
+                    <span className="item-delete" onClick={this.handleDelete}> x</span>
+                </div>
+            </li>
+        )        
+    }
+
+    handleDelete() {
+        this.props.onDelete(this.props.item)
     }
 }
 
@@ -64,3 +102,6 @@ class ListComponent extends React.Component {
 
 // put component into html page
 ReactDOM.render(<TodoComponent />, document.getElementById('todo-wrapper'))
+
+
+// Read events system documentation
