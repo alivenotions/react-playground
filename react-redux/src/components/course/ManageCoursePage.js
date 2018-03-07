19dc2ffad388e9a5cf9812d16e4724b02aebab0a
Bhavdeep <dhanjalbhavdeep@gmail.com>
2018-03-07 14:33:54 +0530

    authors and courses on ManageCoursePage

--- react-redux/src/components/course/ManageCoursePage.js
+++ react-redux/src/components/course/ManageCoursePage.js
@@ -1,27 +1,47 @@
-import React, { PropType } from 'react'
+import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import * as courseActions from '../../actions/courseActions'
+import CourseForm from './CourseForm'
 
 class ManageCoursePage extends React.Component {
     constructor(props, context) {
         super(props, context)
+        this.state = {
+            course: Object.assign({}, props.course),
+            errors: {}
+        }
     }
 
     render() {
         return (
-            <h1>Manage Course</h1>
+            <CourseForm
+                allAuthors={this.props.authors}
+                course={this.state.course}
+                errors={this.state.errors}
+            />
         )
     }
 }
 
 ManageCoursePage.propTypes = {
-
+    course: PropTypes.object.isRequired,
+    authors: PropTypes.array.isRequired
 }
 
 function mapStateToProps(state, ownProps) {
+    let course = {id: '', watchHref: '', title: '', authorId: '', length: '', category: ''}
+
+    const authorsFormattedForDropdown = state.authors.map(author => {
+        return {
+            value: author.id,
+            text: author.firstName + ' ' + author.lastName
+        }
+    })
+
     return {
-        state
+        course,
+        authors: authorsFormattedForDropdown
     }
 }
 
