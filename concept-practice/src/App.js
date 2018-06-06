f75e98abf748846d6bd61ed1760c3807b1d5fc1d
Bhavdeep Dhanjal <dhanjalbhavdeep@gmail.com>
2018-06-06 21:31:00 +0530

    Form handling

--- concept-practice/src/App.js
+++ concept-practice/src/App.js
@@ -2,13 +2,15 @@ import React from 'react'
 
 import Clock from './components/Clock'
 import List from './components/List'
+import Form from './components/Form'
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Clock incrementBy={2}/>
-        <List />
+        {/* <Clock incrementBy={2}/> */}
+        {/* <List /> */}
+        <Form />
       </div>
     )
   }
